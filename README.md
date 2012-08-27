268830358016f877fc4c49dfe7c5ecde99a423d0
John Grubb Atkinson <john@discountgeni.us>
2012-08-26 23:59:17 -0700

    first commit

---
+++ README.md
@@ -0,0 +1,14 @@
+pixeltable
+==========
+
+*pixeltable* is a python script that takes an image file and encodes it as an HTML table of one-pixel-sized cells.  Each individual cell has a bgcolor attribute that replicates the color of the corresponding pixel.
+
+### Usage ###
+
+**python pixeltable.py [name].jpg** will create **[name].html**; a properly formatted HTML file with inline CSS and a table that replicates the image.
+
+Made by John Atkinson
+ 
+[http://discountgeni.us](http://discountgeni.us)
+
+[@discountgenius](http://twitter.com/discountgenius)  
---
+++ pixeltable.py
@@ -0,0 +1,52 @@
+import os, sys
+import Image
+
+imagefile = sys.argv[1]
+f, e = os.path.splitext(imagefile)
+outfile = open(f + ".html", 'w')
+i = Image.open(imagefile)
+
+pixels = i.load()
+width, height = i.size
+
+def triplet(rgb):
+  return format((rgb[0]<<16)|(rgb[1]<<8)|rgb[2], '06x')
+
+print triplet((170, 187, 204))
+
+colors = [[triplet(pixels[x,y]) for y in range(height)] for x in range(width)]
+
+outfile = open(f + ".html", 'w')
+outfile.write("<html>\n")
+outfile.write("  <head>\n")
+outfile.write("    <style type=\"text/css\">\n")
+outfile.write("      /*<![CDATA[*/\n")
+outfile.write("      table.pixeltable\n")
+outfile.write("      {\n")
+outfile.write("        border-spacing: 0;\n")
+outfile.write("        border-collapse: collapse;\n")
+outfile.write("      }\n")
+outfile.write("      td.pixeltd\n")
+outfile.write("      {\n")
+outfile.write("        width: 1px;\n")
+outfile.write("        height: 1px;\n")
+outfile.write("        border: 0;\n")
+outfile.write("        padding: 0;\n")
+outfile.write("      }\n")
+outfile.write("      /*]]>*/\n")
+outfile.write("    </style>\n")
+outfile.write("  </head>\n")
+outfile.write("  <body bgcolor=\"#ffffff\">\n")
+outfile.write("    <table class=\"pixeltable\">\n")
+for y in range(height):
+  outfile.write("      <tr>\n")
+  for x in range(width):
+    outfile.write("        <td class=\"pixeltd\" bgcolor=\"#")
+    outfile.write(colors[x][y])
+    outfile.write("\"></td>\n")
+  outfile.write("      </tr>\n")
+outfile.write("    </table>\n")
+outfile.write("  </body>\n")
+outfile.write("</html>\n")
+outfile.close()
+
